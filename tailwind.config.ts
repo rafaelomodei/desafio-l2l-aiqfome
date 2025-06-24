dd7cdb7a030043d276e89aa7f69dcffd059d45c7
rafaelomodei <rafael.omodei@outlook.com>
2025-06-23 22:16:50 -0300

    feat: added colors

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  extend: {
+    colors: {
+      primary: 'var(--color-primary)',
+      free: 'var(--color-free)',
+      title: 'var(--color-title)',
+      text: 'var(--color-text)',
+      brand: 'var(--color-brand)',
+      success: 'var(--color-success)',
+    },
+  },
+};
