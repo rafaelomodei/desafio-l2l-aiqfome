5001d81e1d466cdb1175a62e0f2b25aa06dce975
rafaelomodei <rafael.omodei@outlook.com>
2025-06-24 03:17:52 -0300

    feat: created footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className='flex flex-col w-full px-4 py-6 gap-1 text-md  font-bold text-center text-[color:var(--color-text-footer)] bg-[color:var(--color-background-card-product)]'>
+      <p>feito com 💜 em maringá-PR</p>
+      <p className='text-lg'>
+        aiqfome.com © 2007-2023 aiqfome LTDA . CNPJ: 09.186.786/0001-58
+      </p>
+    </footer>
+  );
+}
